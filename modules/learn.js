b765861adfbac09d4bfdbea95392ebbdf8dfab2a
StephenKimaru <stephenkimaru351@gmail.com>
2026-03-26 09:31:53 +0300

    Add learn.js for skill-building resources

---
+++ modules/learn.js
@@ -0,0 +1,17 @@
+export default (ctx) => {
+
+  ctx.reply(
+`📚 Learn & Build Skills
+
+💻 Web Development
+https://www.freecodecamp.org
+
+🐍 Python
+https://www.learnpython.org
+
+₿ Crypto
+https://www.coinbureau.com
+
+Learn one thing today.`
+  );
+};
