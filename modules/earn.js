f55b3852eaeb9927acd7dc2c722a187568c584f9
StephenKimaru <stephenkimaru351@gmail.com>
2026-03-26 09:31:23 +0300

    Add earn.js for earning opportunities

---
+++ modules/earn.js
@@ -0,0 +1,20 @@
+export default (ctx) => {
+
+  ctx.reply(
+`💰 Start Earning Today
+
+🔥 Sweatcoin
+https://w2.swcapp.com/i/macreatives
+
+⚡ MEC App
+https://i.mec.me/?c=xu6us1z1
+
+🟣 Pi Network
+https://minepi.com/macreatives
+
+Code: macreatives
+
+Stay consistent.
+Small steps = big results 🚀`
+  );
+};
