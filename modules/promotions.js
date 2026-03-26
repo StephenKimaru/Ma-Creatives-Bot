e3b6f3d5295da1bff373294f0d48053c48633dc6
StephenKimaru <stephenkimaru351@gmail.com>
2026-03-26 09:30:54 +0300

    Add promotions setup function for bot commands

---
+++ modules/promotions.js
@@ -0,0 +1,18 @@
+export function setupPromotions(bot) {
+
+  bot.command("services", async (ctx) => {
+
+    await ctx.reply(
+`🚀 Need a Website?
+
+Ma Creatives Studio builds:
+
+Websites
+Bots
+Branding
+Automation
+
+Contact us today.`
+    );
+  });
+}
